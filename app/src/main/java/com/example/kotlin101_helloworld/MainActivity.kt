0a6df3c2e9181198c17bd10078864f4ad7a2637f
artintcom <artintcom@gmail.com>
2021-01-18 02:02:59 +0300

    mutable immutable değişken

--- app/src/main/java/com/example/kotlin101_helloworld/MainActivity.kt
+++ app/src/main/java/com/example/kotlin101_helloworld/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.kotlin101_helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val messageTextView: TextView = findViewById(R.id.message_tv)
+
+        // Değiştirilemeyen (immutable) değişken tanımı
+        val name = "Toucan"
+
+        // Değiştirilebilir (mutable) değişken tanımı
+        var age = 1
+        age = 5
+
+        messageTextView.text = "Merhaba benim adım $name . Ben $age yaşındayım"
 
     }
 }
